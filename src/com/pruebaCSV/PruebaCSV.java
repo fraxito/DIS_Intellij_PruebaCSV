0f60a3144c79b4d99f30b648949ee0eaf3389ecc
Jorge Cisneros <fraxito@gmail.com>
2022-10-19 18:21:00 +0200

    Versión final, con las dependencias solucionadas

--- src/com/pruebaCSV/PruebaCSV.java
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -10,6 +10,8 @@ public class PruebaCSV {
      */
 
     public static void main(String[] args) {
+        LectorCSV miLector = new LectorCSV();
 
+        miLector.leeCSV("./datos/pokemon.csv");
     }
 }
