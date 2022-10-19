61d5b255ebb0180159ef71cc828995a36a163d7d
Jorge Cisneros <fraxito@gmail.com>
2022-10-19 17:48:38 +0200

    Commit inicial, con el main creado

---
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -0,0 +1,15 @@
+package com.pruebaCSV;
+/*
+Ejemplo muy sencillo de acceso a un fichero .CSV con datos de pokemon
+No utiliza ningún gestor de dependencias
+ */
+
+public class PruebaCSV {
+    /**
+     * @author jorgecisneros
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
